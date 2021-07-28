7514f4a913a8b25fd118287fa35cd6a0ace4bbb6
Nikol Lopez <nikol.oc.96@gmail.com>
2021-07-28 22:12:30 +0000

    update url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f32049e8.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
